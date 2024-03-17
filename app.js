d55bd870b0a7c77e5dc659fabd6bbc413da648f7
Arslan <muhammadarslanrajput12@gmail.com>
2024-03-17 09:29:37 -0700

    java script example

---
+++ app.js
@@ -0,0 +1,26 @@
+// let student=["ali","adnan","akram","haadi","arslan","muhammad","science"]
+// document.write(student[4])
+
+// let luckyNumbers = [5, 7, 9, 11, 15];
+// console.log(luckyNumbers[4]);
+
+// let vowel=prompt("type vowel")
+// let vol=["a","i","o","u","e"]
+// if(vowel==="a"||vowel==="e"||vowel==="i"||vowel==="o"||vowel==="u"){
+//     document.write("this is a vowel.")
+// }
+// else{document.write("this is not vowel.")}
+
+
+let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+let chr = prompt('enter a chr');
+
+if (vowels.indexOf(chr) !== -1) {
+    console.log('this is a vowel.');
+}
+
+
+
+
+
